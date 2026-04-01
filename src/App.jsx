5aaaa61ce6fcb88b60e2b6d3054e438cc213296c
tfshorifulislam <tfshorifulislam@gmail.com>
2026-04-01 17:31:35 +0600

    banner section design

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
+import BannerSection from './Components/BannerSection/BannerSection'
 import NavBar from './Components/NavBar/NavBar'
 function App() {
 
   return (
     <div>
       <NavBar />
+      <BannerSection />
     </div>
   )
 }
