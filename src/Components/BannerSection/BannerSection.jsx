af6569dda4156c193e3cac442d28349327efcba2
tfshorifulislam <tfshorifulislam@gmail.com>
2026-04-01 17:45:18 +0600

    banner section update

--- src/Components/BannerSection/BannerSection.jsx
+++ src/Components/BannerSection/BannerSection.jsx
@@ -2,29 +2,33 @@ import React from 'react';
 import logo from '../../assets/🚀 Ready to elevate your career in tech_ Join StatQuestJourney Hub and gain industry-ready skills in Data and AI through our innovative hybrid training approach! 🌟 Explore the future of tec.jfif';
 const BannerSection = () => {
     return (
-        <div className='flex flex-col-reverse lg:flex-row justify-between items-center mx-auto w-10/12 gap-10 mt-7 lg:mt-30'>
-            <div className='space-y-5 lg:space-y-10'>
-                <div className='space-y-4  max-w-2xl'>
-                    <h1 className=' text-4xl md:text-7xl  font-semibold md:font-bold  text-center md:text-start lg:leading-20'>
-                        <span className='text-[#2664eb]'>Scale Your Business with</span> Smart Design.
-                    </h1>
-                    <p className='text-center md:text-start text-lg md:text-xl md:text-2xl lg:leading-10 '>
-                        We help entrepreneurs and brands grow through modern web technology and world-class UI/UX.
-                    </p>
+
+        <div className='absolute top-15 right-0 left-0'>
+            <div className='flex flex-col-reverse lg:flex-row justify-between items-center mx-auto w-10/12 gap-10 mt-7 lg:mt-30'>
+                <div className='space-y-5 lg:space-y-10'>
+                    <div className='space-y-4  max-w-2xl'>
+                        <h1 className=' text-4xl md:text-7xl  font-semibold md:font-bold  text-center md:text-start lg:leading-20'>
+                            <span className='text-[#2664eb]'>Scale Your Business with</span> Smart Design.
+                        </h1>
+                        <p className='text-center md:text-start text-lg md:text-xl md:text-2xl lg:leading-10 '>
+                            We help entrepreneurs and brands grow through modern web technology and world-class UI/UX.
+                        </p>
+                    </div>
+                    <div className='flex  items-center justify-center lg:justify-start gap-5 lg:gap-9'>
+                        <button className='btn btn-xs sm:btn-sm md:btn-md  btn-primary rounded-full'>Our Portfolio</button>
+                        <button className='btn btn-xs sm:btn-sm md:btn-md btn-outline rounded-full'>Learn More</button>
+                    </div>
                 </div>
-                <div className='flex  items-center justify-center lg:justify-start gap-5 lg:gap-9'>
-                    <button className='btn btn-xs sm:btn-sm md:btn-md  btn-primary rounded-full'>Our Portfolio</button>
-                    <button className='btn btn-xs sm:btn-sm md:btn-md btn-outline rounded-full'>Learn More</button>
-                </div>
+
+
+                <figure className='pulsing-logo'>
+                    <img className='rounded-3xl'
+                        src={logo}
+                        alt="Shoes" />
+                </figure>
             </div>
-
-
-            <figure className='pulsing-logo'>
-                <img className='rounded-3xl'
-                    src={logo}
-                    alt="Shoes" />
-            </figure>
         </div>
+
 
     );
 };
--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const NavBar = () => {
     return (
-        <div className='flex  justify-between items-center w-11/12 mt-5 md:mt-8 mx-auto'>
+        <div className='flex  justify-between items-center w-11/12 mt-5 md:mt-8 mx-auto fixed top-0 bg-white py-5 left-0 right-0  z-50'>
             <div className=''>
                 <h1 className='text-[#1e293b] text-lg lg:text-xl flex flex-col sm:flex-row items-baseline'><span className='text-[#2664eb] font-bold text-2xl lg:text-4xl'>NEXUS</span>DIGITAL</h1>
             </div>
